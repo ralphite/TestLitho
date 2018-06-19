59f49f8028a8fd6719a7f1ed5ad4b6e1d8e7a54d
Yadong Wen <yadongwen@gmail.com>
2018-06-19 16:43:56 -0700

    section tut 2

--- app/src/main/java/com/example/yadong/testlitho/ListSectionSpec.java
+++ app/src/main/java/com/example/yadong/testlitho/ListSectionSpec.java
@@ -1,6 +1,7 @@
 package com.example.yadong.testlitho;
 
 import android.graphics.Color;
+import android.support.v7.widget.LinearLayoutManager;
 
 import com.facebook.litho.annotations.FromEvent;
 import com.facebook.litho.annotations.OnEvent;
@@ -10,11 +11,16 @@ import com.facebook.litho.sections.annotations.GroupSectionSpec;
 import com.facebook.litho.sections.annotations.OnCreateChildren;
 import com.facebook.litho.sections.common.DataDiffSection;
 import com.facebook.litho.sections.common.RenderEvent;
+import com.facebook.litho.sections.common.SingleComponentSection;
+import com.facebook.litho.sections.widget.ListRecyclerConfiguration;
+import com.facebook.litho.sections.widget.RecyclerCollectionComponent;
 import com.facebook.litho.widget.ComponentRenderInfo;
 import com.facebook.litho.widget.RenderInfo;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.facebook.litho.sections.widget.ListRecyclerConfiguration.SNAP_TO_CENTER;
 
 @GroupSectionSpec
 public class ListSectionSpec {
@@ -22,6 +28,22 @@ public class ListSectionSpec {
   @OnCreateChildren
   static Children onCreateChildren(final SectionContext c) {
     return Children.create()
+        .child(
+            SingleComponentSection.create(c)
+                .component(
+                    RecyclerCollectionComponent.create(c)
+                        .disablePTR(true)
+                        .recyclerConfiguration(
+                            new ListRecyclerConfiguration(
+                                LinearLayoutManager.HORIZONTAL, false, SNAP_TO_CENTER))
+                        .section(
+                            DataDiffSection.create(c)
+                                .data(generateData(32))
+                                .renderEventHandler(ListSection.onRender(c))
+                                .build())
+                        .canMeasureRecycler(true))
+                .sticky(true)
+                .build())
         .child(
             DataDiffSection.create(c)
                 .data(generateData(32))
